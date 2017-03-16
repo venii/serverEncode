aaf0e597837fcaacf8dc09e15ebba52d75121dcc
Vinicius <viniciusferreirawk@gmail.com>
2017-03-16 14:10:02 -0300

    fix

--- start_azure.bat
+++ start_azure.bat
@@ -1,4 +1,3 @@
 @echo OFF
-start cmd.exe /k "node %cd%jsmpeg-master/websocket-relay.js 1234 8081 8082"
 start cmd.exe /k "gulp serve"
 start cmd.exe /k "cd C:\Program Files (x86)\Icecast KH && icecast.exe -c icecast.xml"
