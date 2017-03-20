cc184347bc5df375f98c4fd3100e1d91b653bbf1
Vinicius <viniciusferreirawk@gmail.com>
2017-03-20 14:00:33 -0300

    fix

--- start_azure.bat
+++ start_azure.bat
@@ -1,3 +1,3 @@
 @echo OFF
 start cmd.exe /k "gulp serve"
-start cmd.exe /k "cd C:\Program Files (x86)\Icecast KH && icecast.exe -c icecast.xml"
+#start cmd.exe /k "cd C:\Program Files (x86)\Icecast KH && icecast.exe -c icecast.xml"
