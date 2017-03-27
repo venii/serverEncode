d94555a57144a83b688ade671caa9e5dcf95ee17
Vinicius <viniciusferreirawk@gmail.com>
2017-03-27 11:48:22 -0300

    gix

--- daemon.js
+++ daemon.js
@@ -396,7 +396,7 @@ app.get("/server_audio", function(request, response){ //root dir
     }
 
     var exec = require('child_process').exec;
-    exec('node -v', function(error, stdout, stderr) {
+    exec('icecast -c /home/rtec/serverEncode/icecast_linux.xml', function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         
