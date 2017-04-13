aa184f80e724461abe272da96b1920868a0e889a
Vinicius <viniciusferreirawk@gmail.com>
2017-04-13 15:31:35 -0300

    fix

--- daemon.js
+++ daemon.js
@@ -139,8 +139,8 @@ app.get("/abre_relay", function(request, response){ //root dir
                 var params = params.concat(['-map', '0:1',  
                                             '-codec:a','libmp3lame',
                                             
-                                            '-b:a' ,'128k',
                                             '-bufsize', '128k',
+                                            '-ab' ,'128',
                                             '-ar', '44100',
                                             '-ac',  '1',
                                             
@@ -344,10 +344,10 @@ app.get("/encode_video", function(request, response){ //root dir
       var params = params.concat(['-map', '0:1',  
                                   '-codec:a','libmp3lame',
                                   
-                                   '-b:a' ,'128k',
-                                    '-bufsize', '128k',
-                                    '-ar', '44100',
-                                    '-ac',  '1',
+                                  '-bufsize', '128k',
+                                  '-ab' ,'128',
+                                  '-ar', '44100',
+                                  '-ac',  '1',
 
                                   '-f', 'mp3', 
                                   'icecast://camera:camera@localhost:8000/camera_'+request.param('idCamera')+".mp3"
